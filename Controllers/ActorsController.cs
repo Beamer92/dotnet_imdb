76dd2e87e600e06d70f2706a43ff237eaaf68eda
Brendan Woodell <brendan.woodell@gmail.com>
2019-04-10 14:40:17 -0700

    Got PUT working

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using ReplaceJS.Models;
 
 namespace ReplaceJS.Controllers
@@ -14,7 +15,6 @@ namespace ReplaceJS.Controllers
     public class actorsController : ControllerBase
     {
         private readonly ActorsContext _context;
-
         public actorsController(ActorsContext context)
         {   _context = context;
             // if (_context.actors.Count() == 0)             
@@ -58,14 +58,8 @@ namespace ReplaceJS.Controllers
         //     return CreatedAtAction(nameof(GetActor), new { id = newact.id }, newact);
         // }
 
-        public ActionResult<actor> NewActor(actor newact)
+        public ActionResult<actor> NewActor([FromBody]actor newact)
         {
-             using (var reader = new StreamReader(this.Request.Body))
-            {
-                var body = reader.ReadToEnd();
-                Console.WriteLine(body);
-               
-            }
             _context.actors.Add(newact);
             _context.SaveChanges();
             return CreatedAtAction(nameof(GetActor), new { id = newact.id }, newact);
@@ -74,18 +68,28 @@ namespace ReplaceJS.Controllers
         //Updated At doesn't update yet, handle in PUT
 
 
-        //PUT actors/1
-        // [HttpPut("{id}")]
-        // public ActionResult<actor> UpdateActor(int id)
-        // {
-        //     //find the way to grab the request data here
-        //     using (var reader = new StreamReader(Request.Body))
-        //     {
-        //         var body = reader.ReadToEnd();
-        //         Console.WriteLine(body);
-        //         // Do something
-        //     }
-        // }
+        // PUT actors/1
+        [HttpPut("{id}")]
+        [ProducesResponseType(typeof(actor), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        //StatusCodes comes from using Microsoft.AspNetCore.Http;
+        public ActionResult<actor> UpdateActor(int id, [FromBody]actor upact)
+        {
+                var oldact = _context.actors.FirstOrDefault(c => c.id == id);
+
+                if(oldact == null)
+                {
+                    return new NotFoundResult();
+                }
+                oldact.name = upact.name ?? oldact.name;
+                oldact.biography = upact.biography ?? oldact.biography;
+                oldact.profile_url = upact.profile_url ?? oldact.profile_url;
+                oldact.birth_date = upact.birth_date == new DateTime() ? oldact.birth_date : upact.birth_date;
+                oldact.updated_at = upact.updated_at;
+                _context.SaveChanges();
+                return oldact;
+        }
+
 
         //delete actors/1
 
